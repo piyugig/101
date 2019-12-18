1739d08728c4f22234808581ba7f6bb4593d8915
Piyush Gupta <piyush.gig@live.com>
2019-12-18 17:43:08 +0530

    argsString with test

--- 17th-dec/argsString.js
+++ 17th-dec/argsString.js
@@ -1,3 +1,9 @@
-function argsString() {
-    
+function argsString(inputString, replaceWithArray) {
+    return inputString.replace(/{}/g, () => {
+        replaceWith = replaceWithArray[0];
+        replaceWithArray.shift();
+        return replaceWith;
+    });
 }
+
+module.exports = argsString;
