2cdc93549d7644a522f2640664ffe7bf4f06c460
Piyush Gupta <piyush.gig@live.com>
2020-01-07 19:41:39 +0530

    format code

--- 6th-jan/alternatingCharacters.js
+++ 6th-jan/alternatingCharacters.js
@@ -1,18 +1,20 @@
 function alternatingCharacters(ArrayOfStrings) {
-    return ArrayOfStrings.map(string => {
-        return alternatingCharacter(string);
-    });
+  return ArrayOfStrings.map(string => {
+    return alternatingCharacter(string);
+  });
 }
 
 function alternatingCharacter(string) {
-    noOfDelete = 0;
-    return [...string].reduce((totalNoOfDelete, currentValue, currentIndex, array) => {
-        
-        if (currentValue === array[currentIndex+1]){
-            totalNoOfDelete++;
-        }
-        return totalNoOfDelete;
-      }, noOfDelete);
+  noOfDelete = 0;
+  return [...string].reduce(
+    (totalNoOfDelete, currentValue, currentIndex, array) => {
+      if (currentValue === array[currentIndex + 1]) {
+        totalNoOfDelete++;
+      }
+      return totalNoOfDelete;
+    },
+    noOfDelete
+  );
 }
 
 module.exports = alternatingCharacters;
